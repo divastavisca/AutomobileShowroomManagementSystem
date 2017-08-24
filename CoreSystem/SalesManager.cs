2a2e5e267aeb3bd306a344b1b49aa976f59d1d06
divastavisca <dagarwal@tavisca.com>
2017-08-24 15:27:43 +0530

    minor change

--- CoreSystem/SalesManager.cs
+++ CoreSystem/SalesManager.cs
@@ -12,13 +12,15 @@ namespace CoreSystem
 
         public double GetTodaysSale()
         {
-            return _salesRecord[DateTime.Now];
+            return _salesRecord[DateTime.Today];
         }
 
         public void NewSale(double price)
         {
             double roadPrice = price + additionalCost(price);
-            _salesRecord[DateTime.Now] += roadPrice;
+            if (_salesRecord.ContainsKey(DateTime.Today))
+                _salesRecord[DateTime.Today] += roadPrice;
+            else _salesRecord.Add(DateTime.Today, roadPrice);
         }
 
         private double additionalCost(double price)
